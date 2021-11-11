856f734bc773c76a58809ae718ce31f25029e80b
yaoyuchen <927977789@qq.com>
2021-11-11 22:27:07 +0800

    1

--- src/main/java/com/atguigu/User.java
+++ src/main/java/com/atguigu/User.java
@@ -22,7 +22,9 @@ public class User {
 
     public static void main(String[] args) {
         System.out.println(new User().toString());
-        
+        System.out.println(new User().toString());
+        System.out.println(new User().toString());
+
 
     }
 }
